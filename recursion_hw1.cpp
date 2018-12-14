2c71c74e37c4f2274acbf70d7f5ddcf9a398d401
EliezerDY <42982224+EliezerDY@users.noreply.github.com>
2018-12-14 14:12:14 -0500

    Add files via upload

---
+++ recursion_hw1.cpp
@@ -0,0 +1,25 @@
+// recursion_hw1 - Eliezer Brown
+#include <iostream>
+using namespace std;
+   void outputNumberInReverse(int n);
+           int main() {
+    int n;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    cout << "Reversed Number: ";
+    outputNumberInReverse(n);
+
+    cout << endl;
+    return 0;
+}
+
+void outputNumberInReverse(int x) {
+     if(x < 10)
+  cout << x;
+      else {
+    cout << x%10;
+        outputNumberInReverse(x/10);
+    }
+}
