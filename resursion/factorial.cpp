993692506667f2f8f3d126057ca75c3c77e7761f
EliezerDY <42982224+EliezerDY@users.noreply.github.com>
2020-06-10 19:30:58 -0400

    Create factorial.cpp
    recursively return the factorial of a number

---
+++ resursion/factorial.cpp
@@ -0,0 +1,22 @@
+//  Eliezer Brown
+
+#include <iostream>
+using namespace std;
+
+int factorial(int x);
+
+        int main() {
+  int n;
+cout << "Enter a number: ";
+cin >> n;
+
+cout << n << "! = " << factorial(n) << endl;
+cout << endl;
+    return 0;
+}
+
+int factorial(int x) {
+    if (x == 1)
+        return 1;
+    return x * factorial(x - 1);
+}
