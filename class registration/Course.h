4ca0485b1903654068ec73970dbddab9484f9716
EliezerDY <42982224+EliezerDY@users.noreply.github.com>
2020-06-09 22:04:25 -0400

    Add files via upload
    header file for course.cpp

---
+++ class registration/Course.h
@@ -0,0 +1,33 @@
+// project -E.Brown
+#ifndef COURSE_H
+#define COURSE_H
+
+#include <iostream>
+#include "MyArray.h"
+#include "Student.h"
+#include "Instructor.h"
+
+class Student;  // forward declaration of class A
+class Instructor;
+
+class Course {
+public:
+     Course();
+     Course(std::string n);
+     Course(std::string n, Instructor &inob);
+
+    std::string getName() const;
+    void setName(std::string n);
+    Instructor getInstructor() const;
+    void setInstructor(Instructor &inob);
+    void addStudent(Student &sob);
+    void dropStudent(Student &sob);
+    void dropInstructor();
+    void display();
+private:
+   std::string name;
+   Instructor *instructors;//is this right?
+   MyArray<Student*>students;//like this?
+  };
+
+#endif
