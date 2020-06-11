cc027944edaa85791cadedfe4f2e4242b25d1232
EliezerDY <42982224+EliezerDY@users.noreply.github.com>
2020-06-10 21:52:47 -0400

    Create while_loop.cpp
    basic while loop

---
+++ loops/while_loop.cpp
@@ -0,0 +1,20 @@
+//while_loop - Eliezer Brown
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+  cout<<"Write all numbers 0 to 30 divisble by 3 "<<endl;
+    int i;
+
+
+    i = 30;
+    while(i>=0) {
+        cout << i << " ";
+        i-=3;
+    }
+    cout << endl;
+
+    return 0;
+}
