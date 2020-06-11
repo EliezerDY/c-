9f75cc13fcf99be01a9407a5b50ed79ef576b633
EliezerDY <42982224+EliezerDY@users.noreply.github.com>
2020-06-10 21:58:09 -0400

    Update square.cpp
    get numbers and their square roots from 1-10

--- square.cpp
+++ square.cpp
@@ -4,15 +4,9 @@ int main() {
 
   int i = 0;
   int square = 0;
-
-
   while (i < 10) {
-
-  square = i * i;
-
-  std::cout << i << "  " << square << "\n";
-
-  i++;
-
-}
+    square = i * i;
+    std::cout << i << "  " << square << "\n";
+    i++;
+   }
 }
