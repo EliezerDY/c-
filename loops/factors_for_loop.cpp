eb4018c5b489281149e2024932d607f55dae325e
EliezerDY <42982224+EliezerDY@users.noreply.github.com>
2020-06-10 21:56:09 -0400

    Create factors_for_loop.cpp

---
+++ loops/factors_for_loop.cpp
@@ -0,0 +1,18 @@
+//loop_ factors -Eliezer Brown
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+   int num, i;
+   cout<<"Enter a number: ";
+   cin>>num;
+   cout << "Factors of " << num << " are: ";
+     for( i=1; i <= num; i++) {
+        if(num%i == 0)
+          cout<< i<<" ";
+     }
+
+  cout << endl;
+  return 0;
+}
