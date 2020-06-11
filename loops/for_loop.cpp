8a89c7df90b1d618b661ab19636dbc2d308b0d93
EliezerDY <42982224+EliezerDY@users.noreply.github.com>
2020-06-10 21:54:32 -0400

    Create for_loop.cpp
    for loop to display exponent

---
+++ loops/for_loop.cpp
@@ -0,0 +1,15 @@
+//for_loop-Eliezer Brown
+#include <iostream>
+using namespace std;
+int main(){
+    int num, ex, ans=1,i;
+    cout << " Enter a number: ";
+    cin >> num;
+    cout << " Enter an exponent: ";
+	  cin>>ex;
+
+    for (i = 1; i <=ex; i++){
+       ans=ans*num;
+    }
+	     cout <<num<<" to the power of "<<ex<<" is "<<ans<<endl ;
+ }
