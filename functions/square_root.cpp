af55be5fde165623d263e1f599aba26424fa8dd2
EliezerDY <42982224+EliezerDY@users.noreply.github.com>
2020-06-11 21:45:53 -0400

    Create square_root.cpp
    return rounded square root

---
+++ functions/square_root.cpp
@@ -0,0 +1,18 @@
+//-Eliezer Brown
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int main()
+{
+	int y;
+
+  cout<<"Enter a number: ";
+  cin>>y;
+  double result = sqrt(y);
+  int sqrtRoundedUp = ceil(result);
+	cout << "Square root of " << y << " rounded to the nearest integer is " << sqrtRoundedUp <<"."<< endl;
+
+	return 0;
+}
