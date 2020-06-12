fee1e774d99c13bce3e17da92125b4d2e5ba80ab
EliezerDY <42982224+EliezerDY@users.noreply.github.com>
2020-06-11 22:14:50 -0400

    Create int_from_float.cpp
    get int value of floating point

---
+++ variables/int_from_float.cpp
@@ -0,0 +1,17 @@
+//get int value of floating point-Eliezer Brown
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+  int i;
+  double a;
+
+ cout << "Input a floating-point value:";
+ cin >>a;
+ cout <<endl;
+ cout << "Integer data is";
+ cin >>(int)a;
+ return 0;
+ }
