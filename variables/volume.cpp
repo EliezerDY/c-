26edf1753dbd2c3da2d25d88f14e006dc2532220
EliezerDY <42982224+EliezerDY@users.noreply.github.com>
2020-06-11 22:13:19 -0400

    Create volume.cpp
    get volume of a box

---
+++ variables/volume.cpp
@@ -0,0 +1,25 @@
+//volume-Eliezer Brown
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+ int length;
+ int width;
+ int height;
+ cout << "Input the height of a box in inches:";
+ cin >>length;
+ cout <<endl;
+cout << "Input the length of a box in inches:";
+ cin >> width;
+ cout <<endl;
+ cout << "Input the width of a box in inches:";
+ cin >> height;
+ cout <<endl;
+
+ cout <<"the volume of the box is " << length * width * height <<" cubic inches.";
+
+
+return 0;
+}
